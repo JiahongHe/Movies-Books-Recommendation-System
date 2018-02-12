1fd83f083b7872a3e8b699d08ddb327fc45c0b92
Jiahong He <jh3863@columbia.edu>
2018-02-12 17:17:12 -0500

    initial commitment

---
+++ movies/recommmendation/upload_ratings.py
@@ -0,0 +1,22 @@
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication('https://eecs6893-movie-data.firebaseio.com', None)
+
+fileNames = ['sample-ratings']
+users = set([])
+for name in fileNames:
+    f = open(name)
+    for line in f.readlines():
+        infos = line.split(',')
+        userId = infos[0]
+        movieId = infos[1]
+        rating = infos[2]
+        firebase.post('/user_rating/'+userId+'/'+movieId,rating)
+        users.add(userId)
+    f.close()
+
+f = open('rated-users','w')
+for user in users:
+    f.write(user)
+    f.write('\n')
+f.close()
